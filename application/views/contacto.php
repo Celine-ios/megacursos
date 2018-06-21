e77c3b8abc89d932b110cbf15f7d1d9366b49b6f
wh0am123 <thejyjco@gmail.com>
2018-06-21 13:13:49 -0430

    Juan was here

--- application/views/contacto.php
+++ application/views/contacto.php
@@ -5,23 +5,35 @@
 	<title>Contacto</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.js"></script>
+<style>
+a {
+color: #fff;
+}
+a:hover {
+font-weight: bolder;
+}
+</style>
 </head>
 <body>
-<form>
-  <div class="grid-container">
-    <div class="grid-x grid-padding-x">
-      <div class="medium-6 cell">
-        <label>Input Label
-          <input type="text" placeholder=".medium-6.cell">
-        </label>
-      </div>
-      <div class="medium-6 cell">
-        <label>Input Label
-          <input type="text" placeholder=".medium-6.cell">
-        </label>
-      </div>
-    </div>
-  </div>
-</form>
+<nav style="display: flex;flex-direction: row;background: #333;padding: 2%;">
+	<ul class="menu align-left">
+	<img src="https://megacursos.com/themes/megacursos/assets/images/logo.png">
+</ul>
+<ul class="menu align-right" style="padding-top: 1%;margin-left: 30%;">
+	<li><a href="#">Clases Gratis</a></li>
+  <li><a href="#">Megacursos</a></li>
+  <li><a href="#">Descarga</a></li>
+  <li><a href="#">Clientes</a></li>
+  <li><a href="#">Contacto</a></li>
+</ul>
+</nav>
+ <section class="grid-x" style="height: 20em;">
+ 	<div class="cell" style="background-image: url('https://assets.jtdwjcwq6f4wp4ce.com/bundles/y4trfx/less-autopilot/images/hero/san-jose.jpg');display: flex;justify-content: center;background-size: cover;background-repeat: no-repeat;background-position: center;">
+ 		<h2 style="color: #fff;margin-top: 8%;">Contáctanos <br>
+ 			<small>Acelera tu aprendizaje contactando con nosotros</small>
+ 		</h2>
+ 	</div>
+ </section>
+
 </body>
 </html>
