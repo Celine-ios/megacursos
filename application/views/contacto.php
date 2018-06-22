5a7eead45712a8376af3d55128a862741e38be65
wh0am123 <thejyjco@gmail.com>
2018-06-21 19:59:22 -0430

    Juan was here

--- application/views/contacto.php
+++ application/views/contacto.php
@@ -5,6 +5,7 @@
 	<title>Contacto</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 <style>
 a {
 color: #fff;
@@ -12,10 +13,16 @@ color: #fff;
 a:hover {
 font-weight: bolder;
 }
+a i {
+	color: #fff;
+}
+a i:hover {
+	color: #333;
+}
 </style>
 </head>
 <body>
-<nav style="display: flex;flex-direction: row;background: #333;padding: 2%;">
+<nav style="display: flex;flex-direction: row;background: #333;padding: 2%;position: fixed;z-index: 1;width: 100%;">
 	<ul class="menu align-left">
 	<img src="https://megacursos.com/themes/megacursos/assets/images/logo.png">
 </ul>
@@ -27,13 +34,103 @@ font-weight: bolder;
   <li><a href="#">Contacto</a></li>
 </ul>
 </nav>
- <section class="grid-x" style="height: 20em;">
- 	<div class="cell" style="background-image: url('https://assets.jtdwjcwq6f4wp4ce.com/bundles/y4trfx/less-autopilot/images/hero/san-jose.jpg');display: flex;justify-content: center;background-size: cover;background-repeat: no-repeat;background-position: center;">
- 		<h2 style="color: #fff;margin-top: 8%;">Contáctanos <br>
+ <section class="grid-y" style="height: 30em;padding-top: 8%;">
+ 	<div class="cell" style="background-image: url('https://assets.jtdwjcwq6f4wp4ce.com/bundles/y4trfx/less-autopilot/images/hero/san-jose.jpg');display: flex;flex-direction: column;align-items: center;background-size: cover;background-repeat: no-repeat;background-position: center;">
+ 		<h2 style="color: #fff;margin: 8% 0%;">Contáctanos<br>
  			<small>Acelera tu aprendizaje contactando con nosotros</small>
  		</h2>
+ 		<footer style="width: 100%;display: flex;justify-content: center;margin-bottom: 2%;">
+ 		<div style="width: 15%;display: flex;justify-content: space-between;">
+ 			<a href="#">
+ 				<i class="fab fa-facebook-square"></i>
+ 			</a>
+ 			<a href="#">
+ 				<i class="fab fa-instagram"></i>
+ 				</a>
+ 			<a href="#">
+ 				<i class="fab fa-twitter"></i>
+ 				</a>
+ 		</div>
+ 	</footer>
  	</div>
  </section>
-
+ <br>
+<section class="x-grid" style="display: flex;align-items: center;flex-direction: column;text-align: center;margin-top: 2%;">
+	<div>
+		<h2>Consulta con Nosotros <br>
+			<small>Hazlo de manera rápida</small>
+		</h2>
+	</div>
+	<br>
+	<div style="margin-top: 6%;display: flex;flex-direction: row;width: 60%;">
+		<div class="align-left">
+		<form>
+			<header>
+				<h4>Categoría</h4>
+			</header>
+			<br>
+	<div class="grid-container">
+    <div class="grid-y grid-padding-y">
+      <div class="small-6 cell">
+        <label>Nombre
+          <input type="text" placeholder=".medium-6.cell">
+        </label>
+      </div>
+      <div class="small-6 cell">
+        <label>Detalles
+          <input type="text" placeholder=".medium-6.cell">
+        </label>
+      </div>
+      <div class="small-6 cell">
+        <label>Status
+          <input type="text" placeholder=".medium-6.cell">
+        </label>
+      </div>
+       <div class="small-6 cell">
+        <button type="submit" class="button large" style="color: #fff;">Consultar</button>
+      </div>
+    </div>
+  </div>
+		</form>
+		</div>
+		<hr style="border: 1px solid #ddd;height: 25em;">
+	<div class="align-right">
+		<header>
+				<h4>Cursos Disponibles</h4>
+			</header>
+			<br>
+	<div class="grid-container">
+    <div class="grid-y grid-padding-y">
+    </div>
+  </div>
+	</div>		
+	</div>
+</section>
+<footer class="grid-y" style="background: #333;display: flex;flex-direction: row;margin-top: 8%;">
+	<div class="cell small-6" style="width: 33.3%;display: flex;justify-content: center;padding: 2% 0%;">
+	<ul>
+		<li>Fuck</li>
+		<li>My</li>
+		<li>Life</li>
+		<li>:(</li>
+	</ul>
+	</div>
+	<div class="cell small-6" style="width: 33.3%;display: flex;justify-content: center;padding: 2% 0%;">
+	<ul>
+		<li>Fuck</li>
+		<li>My</li>
+		<li>Life</li>
+		<li>:(</li>
+	</ul>
+	</div>
+	<div class="cell small-6" style="width: 33.4%;display: flex;justify-content: center;padding: 2% 0%;">
+	<ul>
+		<li>Fuck</li>
+		<li>My</li>
+		<li>Life</li>
+		<li>:(</li>
+	</ul>
+	</div>
+</footer>
 </body>
 </html>
